0d982edb3c192cfe5bd7cbdc7f17e9c68539d574
Ilgova <ilgova.1594@mail.ru>
2023-01-19 04:55:19 +0300

    first commit

---
+++ example002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 2 числа, проверим какое число большее, а какое меньшее");
+Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine() );
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine() );
+
+if (number1 < number2)
+{
+Console.WriteLine($"Число {number1} меньше числа {number2}");
+}
+else 
+{
+Console.WriteLine($"Число {number1} больше числа {number2}");    
+}

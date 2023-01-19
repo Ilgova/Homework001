88bb5e82dbd0d27746d1e34e6efbb17d378d37dd
Ilgova <ilgova.1594@mail.ru>
2023-01-19 05:11:12 +0300

    example008

---
+++ example008/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Все чётные числа от 1 до N");
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+Console.WriteLine($"Все чётные числа от 1 до {N}");
+if (N > 1)
+{
+    while (i <= N)
+    {
+    Console.Write(i + " ");
+    i = i + 2;
+    }
+}

8906e8380af43c30d3a8397bc8f0dbc2e8d73c10
Ilgova <ilgova.1594@mail.ru>
2023-01-19 05:06:57 +0300

    example006

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 3 числа, найдем максимальное из этих чисел");
+Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine() );
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine() );
+Console.WriteLine("Введите 3 число");
+int number3 = Convert.ToInt32(Console.ReadLine() );
+
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+
+}
+Console.WriteLine($"Максимальное число из чисел {number1}, {number2}, {number3} равно {max}");
---
+++ example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Проверка на чётность");
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+
+if ((number1 % number2) == 0)
+{
+Console.WriteLine($"{number1} - ДА ");
+}
+else 
+{
+   Console.WriteLine($"{number1} - НЕТ "); 
+}
